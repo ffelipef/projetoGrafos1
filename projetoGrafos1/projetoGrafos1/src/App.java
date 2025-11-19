9e08f148542323631bebfbb848a78f0924fa64fa
Felipe F <felipefreitas1211@gmail.com>
2025-11-19 18:57:50 -0300

    adicionei 21 dados

--- projetoGrafos1/projetoGrafos1/src/App.java
+++ projetoGrafos1/projetoGrafos1/src/App.java
@@ -9,6 +9,19 @@ public class App {
         arvore.insert(16);
         arvore.insert(25);
         arvore.insert(27);
+        arvore.insert(14);
+        arvore.insert(45);
+        arvore.insert(47);
+        arvore.insert(70);
+        arvore.insert(61);
+        arvore.insert(7);
+        arvore.insert(65);
+        arvore.insert(16);
+        arvore.insert(41);
+        arvore.insert(78);
+        arvore.insert(47);
+        arvore.insert(13);
+        arvore.insert(21);
         
         arvore.print();
 
